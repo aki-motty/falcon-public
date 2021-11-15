b0a9aac0ff36f2b5c52a2950b04760644bec7902
aki-motty <a.yoshimasa@gmail.com>
2021-11-16 05:15:06 +0900

    To update the repositories of each party

--- src/FCLayer.cpp
+++ src/FCLayer.cpp
@@ -4,123 +4,116 @@
 #include "Functionalities.h"
 using namespace std;
 
-FCLayer::FCLayer(FCConfig* conf, int _layerNum)
-:Layer(_layerNum),
- conf(conf->inputDim, conf->batchSize, conf->outputDim),
- activations(conf->batchSize * conf->outputDim), 
- deltas(conf->batchSize * conf->outputDim),
- weights(conf->inputDim * conf->outputDim),
- biases(conf->outputDim)
+FCLayer::FCLayer(FCConfig *conf, int _layerNum)
+    : Layer(_layerNum),
+      conf(conf->inputDim, conf->batchSize, conf->outputDim),
+      activations(conf->batchSize * conf->outputDim),
+      deltas(conf->batchSize * conf->outputDim),
+      weights(conf->inputDim * conf->outputDim),
+      biases(conf->outputDim)
 {
-	initialize();
+  initialize();
 }
-
 
 void FCLayer::initialize()
 {
-	//Initialize weights and biases here.
-	//Ensure that initialization is correctly done.
-    // for random value
-	size_t lower = 30;
-	size_t higher = 50;
-	size_t decimation = 10000;
-	size_t size = weights.size();
+  // Initialize weights and biases here.
+  // Ensure that initialization is correctly done.
+  //  for random value
+  size_t lower = 30;
+  size_t higher = 50;
+  size_t decimation = 10000;
+  size_t size = weights.size();
 
-	// RSSVectorMyType temp(size);
-	// for (size_t i = 0; i < size; ++i)
-	// 	temp[i] = floatToMyType((float)(rand() % (higher - lower) + lower)/decimation);
+  // RSSVectorMyType temp(size);
+  // for (size_t i = 0; i < size; ++i)
+  // 	temp[i] = floatToMyType((float)(rand() % (higher - lower) + lower)/decimation);
 
-	// if (partyNum == PARTY_S)
-	// 	for (size_t i = 0; i < size; ++i)
-	// 		weights[i] = temp[i];
-	// else if (partyNum == PARTY_A or partyNum == PARTY_D)
-	// 	for (size_t i = 0; i < size; ++i)
-	// 		weights[i] = temp[i];
-	// else if (partyNum == PARTY_B or partyNum == PARTY_C)		
-	// 	for (size_t i = 0; i < size; ++i)
-	// 		weights[i] = 0;
-		
-	
-	fill(biases.begin(), biases.end(), make_pair(0,0));
+  // if (partyNum == PARTY_S)
+  // 	for (size_t i = 0; i < size; ++i)
+  // 		weights[i] = temp[i];
+  // else if (partyNum == PARTY_A or partyNum == PARTY_D)
+  // 	for (size_t i = 0; i < size; ++i)
+  // 		weights[i] = temp[i];
+  // else if (partyNum == PARTY_B or partyNum == PARTY_C)
+  // 	for (size_t i = 0; i < size; ++i)
+  // 		weights[i] = 0;
+
+  fill(biases.begin(), biases.end(), make_pair(0, 0));
 }
-
 
 void FCLayer::printLayer()
 {
-	cout << "----------------------------------------------" << endl;  	
-	cout << "(" << layerNum+1 << ") FC Layer\t\t  " << conf.inputDim << " x " << conf.outputDim << endl << "\t\t\t  "
-		 << conf.batchSize << "\t\t (Batch Size)" << endl;
+  cout << "----------------------------------------------" << endl;
+  cout << "(" << layerNum + 1 << ") FC Layer\t\t  " << conf.inputDim << " x " << conf.outputDim << endl
+       << "\t\t\t  "
+       << conf.batchSize << "\t\t (Batch Size)" << endl;
 }
-
 
 void FCLayer::forward(const RSSVectorMyType &inputActivation)
 {
-	log_print("FC.forward");
+  log_print("FC.forward");
 
-	size_t rows = conf.batchSize;
-	size_t columns = conf.outputDim;
-	size_t common_dim = conf.inputDim;
-	size_t size = rows*columns;
+  size_t rows = conf.batchSize;
+  size_t columns = conf.outputDim;
+  size_t common_dim = conf.inputDim;
+  size_t size = rows * columns;
 
-	if (FUNCTION_TIME)
-		cout << "funcMatMul: " << funcTime(funcMatMul, inputActivation, weights, activations, rows, common_dim, columns, 0, 0, FLOAT_PRECISION) << endl;
-	else
-		funcMatMul(inputActivation, weights, activations, rows, common_dim, columns, 0, 0, FLOAT_PRECISION);
+  if (FUNCTION_TIME)
+    cout << "funcMatMul: " << funcTime(funcMatMul, inputActivation, weights, activations, rows, common_dim, columns, 0, 0, FLOAT_PRECISION) << endl;
+  else
+    funcMatMul(inputActivation, weights, activations, rows, common_dim, columns, 0, 0, FLOAT_PRECISION);
 
-
-	for(size_t r = 0; r < rows; ++r)
-		for(size_t c = 0; c < columns; ++c)
-			activations[r*columns + c] = activations[r*columns + c] + biases[c];
+  for (size_t r = 0; r < rows; ++r)
+    for (size_t c = 0; c < columns; ++c)
+      activations[r * columns + c] = activations[r * columns + c] + biases[c];
 }
 
-
-void FCLayer::computeDelta(RSSVectorMyType& prevDelta)
+void FCLayer::computeDelta(RSSVectorMyType &prevDelta)
 {
-	log_print("FC.computeDelta");
+  log_print("FC.computeDelta");
 
-	//Back Propagate	
-	size_t rows = conf.batchSize;
-	size_t columns = conf.inputDim;
-	size_t common_dim = conf.outputDim;
-	
+  // Back Propagate
+  size_t rows = conf.batchSize;
+  size_t columns = conf.inputDim;
+  size_t common_dim = conf.outputDim;
 
-	if (FUNCTION_TIME)
-		cout << "funcMatMul: " << funcTime(funcMatMul, deltas, weights, prevDelta, rows, common_dim, columns, 0, 1, FLOAT_PRECISION) << endl;
-	else
-		funcMatMul(deltas, weights, prevDelta, rows, common_dim, columns, 0, 1, FLOAT_PRECISION);
+  if (FUNCTION_TIME)
+    cout << "funcMatMul: " << funcTime(funcMatMul, deltas, weights, prevDelta, rows, common_dim, columns, 0, 1, FLOAT_PRECISION) << endl;
+  else
+    funcMatMul(deltas, weights, prevDelta, rows, common_dim, columns, 0, 1, FLOAT_PRECISION);
 }
 
-
-void FCLayer::updateEquations(const RSSVectorMyType& prevActivations)
+void FCLayer::updateEquations(const RSSVectorMyType &prevActivations)
 {
-	log_print("FC.updateEquations");
+  log_print("FC.updateEquations");
 
-	size_t rows = conf.batchSize;
-	size_t columns = conf.outputDim;
-	size_t common_dim = conf.inputDim;
-	size_t size = rows*columns;	
-	RSSVectorMyType temp(columns, std::make_pair(0,0));
+  size_t rows = conf.batchSize;
+  size_t columns = conf.outputDim;
+  size_t common_dim = conf.inputDim;
+  size_t size = rows * columns;
+  RSSVectorMyType temp(columns, std::make_pair(0, 0));
 
-	//Update Biases
-	for (size_t i = 0; i < rows; ++i)
-		for (size_t j = 0; j < columns; ++j)
-			temp[j] = temp[j] + deltas[i*columns + j];
+  // Update Biases
+  for (size_t i = 0; i < rows; ++i)
+    for (size_t j = 0; j < columns; ++j)
+      temp[j] = temp[j] + deltas[i * columns + j];
 
-	funcTruncate(temp, LOG_MINI_BATCH + LOG_LEARNING_RATE, columns);
-	subtractVectors<RSSMyType>(biases, temp, biases, columns);
+  funcTruncate(temp, LOG_MINI_BATCH + LOG_LEARNING_RATE, columns);
+  subtractVectors<RSSMyType>(biases, temp, biases, columns);
 
-	//Update Weights 
-	rows = conf.inputDim;
-	columns = conf.outputDim;
-	common_dim = conf.batchSize;
-	size = rows*columns;
-	RSSVectorMyType deltaWeight(size);
+  // Update Weights
+  rows = conf.inputDim;
+  columns = conf.outputDim;
+  common_dim = conf.batchSize;
+  size = rows * columns;
+  RSSVectorMyType deltaWeight(size);
 
-	if (FUNCTION_TIME)
-		cout << "funcMatMul: " << funcTime(funcMatMul, prevActivations, deltas, deltaWeight, rows, common_dim, columns, 1, 0, FLOAT_PRECISION + LOG_LEARNING_RATE + LOG_MINI_BATCH) << endl;
-	else
-		funcMatMul(prevActivations, deltas, deltaWeight, rows, common_dim, columns, 1, 0, 
-					FLOAT_PRECISION + LOG_LEARNING_RATE + LOG_MINI_BATCH);
-	
-	subtractVectors<RSSMyType>(weights, deltaWeight, weights, size);		
+  if (FUNCTION_TIME)
+    cout << "funcMatMul: " << funcTime(funcMatMul, prevActivations, deltas, deltaWeight, rows, common_dim, columns, 1, 0, FLOAT_PRECISION + LOG_LEARNING_RATE + LOG_MINI_BATCH) << endl;
+  else
+    funcMatMul(prevActivations, deltas, deltaWeight, rows, common_dim, columns, 1, 0,
+               FLOAT_PRECISION + LOG_LEARNING_RATE + LOG_MINI_BATCH);
+
+  subtractVectors<RSSMyType>(weights, deltaWeight, weights, size);
 }
